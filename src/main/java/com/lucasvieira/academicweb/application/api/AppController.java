6f44884448b9313f6e0a89a81ae2e550ed945f56
Lucas Teixeira Vieira <lucas.teixeira34@gmail.com>
2022-05-07 16:59:27 -0300

    Iniciando projeto com homepage.

---
+++ src/main/java/com/lucasvieira/academicweb/application/api/AppController.java
@@ -0,0 +1,13 @@
+package com.lucasvieira.academicweb.application.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping
+    public String homepage() {
+        return "homepage/index";
+    }
+}
---
+++ src/main/resources/static/js/notas.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#dataTable tbody tr td").each(function () {
+        if ($(this).text() > 5 && $(this).text() <= 10)
+            $(this).addClass('verde');
+        else if ($(this).text() > 0.1 && $(this).text() <= 5)
+            $(this).addClass('vermelho');
+    });
+    
+});
